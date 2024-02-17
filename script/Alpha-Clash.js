bfe48ed224947d2ca9a135d367e1e4b5afb5afb3
Noman Mia <nomanislamnishad@gmail.com>
2024-02-17 09:36:17 +0600

    current score

--- script/Alpha-Clash.js
+++ script/Alpha-Clash.js
@@ -7,15 +7,51 @@
 //     const playGroundSection=document.getElementById('play-ground')
 //     playGroundSection.classList.remove('hidden');
 // }
+function handleKeyboardKeyUpEvent(event){
+    const playerPressed=event.key
+    console.log('player pressed',playerPressed);
+
+    //get the expected to press 
+    const currentAlphabetElement=document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText
+    const expectedAlphabet=currentAlphabet.toLowerCase();
+    console.log(playerPressed,expectedAlphabet);   
+    //check match or not
+    if(playerPressed===expectedAlphabet){
+        console.log('you got a point!');
+        //update score:
+        //1.get the current score
+        const currentScoreElemnet=document.getElementById("current-score");
+        const currentScoreText=currentScoreElemnet.innerText;
+        const currentScore=parseInt(currentScoreText);
+        console.log(currentScore);
+        //2.increse the score by 1
+        const newScore=currentScore+1
+        //3.show the updated score
+        currentScoreElemnet.innerText =newScore;
+
+        //start a new round
+        console.log('you have pressed correctly',expectedAlphabet);
+        removeBackgroundColorById(expectedAlphabet);
+        continueGame();
+
+    }else{
+        console.log("dhurr baiya ba apu right key press kor");
+    }
+}
+
+document.addEventListener('keyup',handleKeyboardKeyUpEvent); 
+
 function continueGame(){
 //step-1:generate a random alphabet
 const alphabet=getARandomAlphabet();
-console.log('your random alphabet:',alphabet);
+// console.log('your random alphabet:',alphabet);
 // set rendomley genarate alphabet to the screen(show it)
 const currentAlphabetElement=document.getElementById('current-alphabet')
 currentAlphabetElement.innerText=alphabet;
 setBackgroundColorById(alphabet);
 }
+
 function play(){
     hideElementById('home-screen');
     showElementById('play-ground');
